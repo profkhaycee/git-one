c1baa3b27da3fe09f850c009451f89b9be24360e
Stephen Kelechi Emmanuel <75072261+profkhaycee@users.noreply.github.com>
2022-09-26 14:55:01 +0100

    Update send_mail_from_cpanel.php

--- send_mail_from_cpanel.php
+++ send_mail_from_cpanel.php
@@ -20,29 +20,8 @@ require '/home/kdclovee/public_html/PHPMailer/src/SMTP.php';
 require '/home/kdclovee/public_html/PHPMailer/src/Exception.php';
 
 
-//Create a new PHPMailer instance
-// $mail = new PHPMailer();
-// $mail->isSMTP();
-//Set who the message is to be sent from
-// $mail->setFrom('info@kdcloveembassy.com', 'First Last');
-// //Set an alternative reply-to address
-// $mail->addReplyTo('info@kdcloveembassy.com', 'rst Last');
-// //Set who the message is to be sent to
-// $mail->addAddress('apprealms@gmail.com', 'John Doe');
-// //Set the subject line
-// $mail->Subject = 'PHPMailer mail() test';
-// //Read an HTML message body from an external file, convert referenced images to embedded,
-// //convert HTML into a basic plain-text alternative body
-// // $mail->msgHTML(file_get_contents('contents.html'), __DIR__);
-// $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-
-// //Replace the plain text body with one created manually
-// $mail->AltBody = 'This is a plain-text message body';
-// //Attach an image file
-// // $mail->addAttachment('images/phpmailer_mini.png');
 /*
-$mysql = mysqli_connect('localhost', 'kdclovee_kdc', 'E.-[1SW355TA]Vp(');
-mysqli_select_db($mysql, 'kdclovee_kdc');
+
 $query = "select DISTINCT email, name from (SELECT fullname as name, email FROM `bc21` union select name, email from bc21mailinglist) as mails group by email limit 250, 50";
 $result = mysqli_query($mysql, $query);
 
